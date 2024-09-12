4a0fcf4c3beb85bdba68dcb08b45dc1f9fd9cf87
EngineerLucky <lucksenhlatshwayo19@gmail.com>
2024-09-12 19:11:53 +0000

    0x00personaldata

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+The module for encrypting passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    The function hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    The function checks is a hashed password was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
